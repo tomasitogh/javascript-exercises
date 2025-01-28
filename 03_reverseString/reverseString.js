8de06aef0e8624b159a1eaef4546f4a11070cc53
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBookAir.fibertel.com.ar>
2025-01-28 16:33:52 -0300

    Exercise 03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseWords = '';
+    for (let i = string.length - 1; i >= 0; i--){
+        reverseWords += string[i];
+    }
+    return reverseWords;
 };
 
 // Do not edit below this line
