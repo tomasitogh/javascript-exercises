e4b1cd4fd5c68a59734084dad87d96e64d811744
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBookAir.fibertel.com.ar>
2025-01-28 16:12:35 -0300

    Exercise 02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let completeString = '';
+    if (num < 0){
+        return "ERROR";
+    }
+    for(let i = 1; i <= num; i++){
+        completeString += string;
+    }
+    return completeString;
 };
 
 // Do not edit below this line
