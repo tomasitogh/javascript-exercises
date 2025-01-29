f7d7b3ca91a94cb375fb14e681d486225713811c
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBookAir.fibertel.com.ar>
2025-01-29 12:27:56 -0300

    Exercise 07 done, end of this section

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  let tempConvertedToCelsius = parseFloat((temperatureInFahrenheit - 32) * (5/9));
+  return parseFloat(tempConvertedToCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  let tempConvertedToFahrenheit = parseFloat((temperatureInCelsius) * (9/5) + 32);
+  return parseFloat(tempConvertedToFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
