2ebfb798aba3c5bbe51485ce74e463bcedb2d4cb
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBookAir.fibertel.com.ar>
2025-01-29 10:55:13 -0300

    Exercise 05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    //Corrobates if the input numbers are positive integers. If not, it returns 'ERROR'.
+    if (num1 < 0 || num2 < 0 || typeof num1 != 'number' && isNaN(num1) || typeof num2 != 'number' && isNaN(num2) || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
+    if (num1 > num2){
+        let newNum1 = num2;
+        num2 = num1;
+        num1 = newNum1;
+    }
+    for (let i = num1; i <= num2; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
