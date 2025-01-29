6802dc479c4947e663ff76e3786a5cbe34e637d3
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBookAir.fibertel.com.ar>
2025-01-29 10:29:46 -0300

    Exercise 04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    const arrayRemoved = [];
+    for (let element of array){
+        if (!elementsToRemove.includes(element)){
+            arrayRemoved.push(element);
+        }
+    }
+    return arrayRemoved;
 };
 
 // Do not edit below this line
