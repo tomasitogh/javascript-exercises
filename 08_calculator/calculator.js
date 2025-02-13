b33282da7e8ba01b407ebb6e9aa8cf563cf2828e
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBookAir.fibertel.com.ar>
2025-02-13 17:14:39 -0300

    Exercise 08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr[0]=== undefined){
+    return 0;
+  }
+	return arr.reduce((acc, act) => {
+    return acc + act;
+  })
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, act) => {
+    return acc * act;
+  })
 };
 
-const power = function() {
-	
+const power = function(num1, numPower) {
+	let power = 1;
+  for (let i = 1; i <= numPower; i++){
+    power *= num1;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let fact = 1;  
+  for (let i = num1; i >= 1; i--){
+    fact *= i; 
+  }
+	return fact;
 };
 
 // Do not edit below this line
