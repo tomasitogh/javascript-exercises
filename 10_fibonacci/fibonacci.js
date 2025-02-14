3f8206d767810406e9657283e27628629ed69bd6
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBook-Air-de-Tomas.local>
2025-02-14 13:22:09 -0300

    Exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0){
+        return "OOPS";
+    } else if (num == 0){
+        return 0;
+    }
 
+    const fibo = [1, 1];
+    for (let i = 2; i < num;i++){
+        fibo[i] = fibo[i - 2] + fibo[i - 1];
+    }
+    return fibo[num-1];
 };
 
 // Do not edit below this line
