624aab9353cc6b830cd3ee83060432a53dd6ff8f
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBook-Air-de-Tomas.local>
2025-02-14 14:20:13 -0300

    Exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if (oldestAge > currentAge){
+            return oldestPerson;
+        }else {
+            return currentPerson;
+        }
+    })
 };
+function getAge(birthDate, deathDate){
+    if (deathDate == null){
+        deathDate = new Date().getFullYear();
+    }
+    return deathDate - birthDate;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
